2dbd1ff6a16b4eaf10178d5c64840d8670156ea9
Alberto <alberto@nada.com>
2022-05-17 16:52:56 +0200

    funciones

--- js/funciones.js
+++ js/funciones.js
@@ -1,5 +1,15 @@
 //Funciones de mi programas
+
+let a=20;
+
 function holaMundo() {
-    document.write('<h1> MI PRIMERA FUNCION </h1');
+    let mensaje1='<h1> MI PRIMERA FUNCION </h1';
+    console.log(mensaje1);
+   console.log("en la funcion:"+a);
+}
+
+function pintar_mensaje(mensaje)
+{   
+     console.log(mensaje);
 
 }
